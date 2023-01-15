cdf5d2c0c42da7b998ca79efea2f645e698f31ce
CarolUrbs <caroline.urbainski@edu.univali.br>
2023-01-15 19:38:33 -0300

    Movimento e Animação
    Implementação Movimentação e Animação

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,16 @@ using Ebac.StateMachine;
 
 public class Player :Singleton<Player>
 {
+    public Animator animator;
+    public CharacterController characterController;
+    public float speed =1f;
+    public float turnSpeed =1f;
+    public float gravity = -9.8f;
+    public float jumpSpeed = 15f;
+    public float vSpeed = 4f;
+    [Header("Run Setup")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float speedRun = 40f;
    public enum PlayerState
     {
         IDLE,
@@ -41,4 +51,80 @@ public class Player :Singleton<Player>
 
     }
 
+    private void Update()
+    {
+       
+        
+            transform.Rotate(0, Input.GetAxis("Horizontal") *turnSpeed * Time.deltaTime, 0);
+
+            var inputAxisVertical = Input.GetAxis("Vertical");
+            var speedVector = transform.forward * Input.GetAxis("Vertical") * speed;
+
+            if(characterController.isGrounded)
+              {
+            vSpeed = 0;
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                vSpeed = jumpSpeed;
+                animator.SetBool("Jump", !characterController.isGrounded);
+
+            }
+        }
+        var isWalking = inputAxisVertical != 0;
+        if(isWalking)
+        {
+            if(Input.GetKey(keyRun))
+            {
+                speedVector *= speedRun;
+                animator.speed = speedRun;
+            }
+            else
+            {
+                animator.speed = 1;
+
+            }
+        }
+            vSpeed -= gravity * Time.deltaTime;
+            speedVector.y = vSpeed;
+            characterController.Move(speedVector*Time.deltaTime);
+
+            animator.SetBool("Run", isWalking);
+    }
+
+
+
+}
+namespace PlayerStates
+{
+
+    public class PS_Idle : StateBase
+    {
+   
+       
+        
+    }
+    public class PS_Walking : StateBase
+    {
+       
+
+    }
+
+    public class PS_Running : StateBase
+    {
+
+    }
+
+    public class PS_Jumping : StateBase
+    {
+
+    }
+
+    public class PS_Shooting : StateBase
+    {
+
+    }
+    public class PS_Dead : StateBase
+    {
+
+    }
 }
