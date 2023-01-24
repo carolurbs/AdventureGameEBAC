3c4c6848dd9af9a5ea755874fe53b895d63a5d8a
CarolUrbs <caroline.urbainski@edu.univali.br>
2023-01-24 16:09:54 -0300

    Patrulha Inimigos
    Consertada a patrulha dos inimigos

--- Assets/Scripts/Enemies/EnemyWalk.cs
+++ Assets/Scripts/Enemies/EnemyWalk.cs
@@ -23,9 +23,9 @@ public class EnemyWalk : EnemyBase
                     _index = 0;
                 }
 
+            }
                 transform.position = Vector3.MoveTowards(transform.position, waypoints[_index].transform.position, Time.deltaTime * speed);
                 transform.LookAt(waypoints[_index].transform.position);
-            }
         }
 
     }
