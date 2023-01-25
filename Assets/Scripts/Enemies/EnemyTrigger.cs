5439d44acdbbf98baaef5df3fcf1508919c8f6e1
CarolUrbs <caroline.urbainski@edu.univali.br>
2023-01-25 12:33:06 -0300

    Trigger inimigo e boss
    Inimigos e o Boss são ativados e desativados através de um  trigger.

---
+++ Assets/Scripts/Enemies/EnemyTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour
+{
+    public GameObject enemyType;
+
+
+    public void Awake()
+    {
+        enemyType.SetActive(false);
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            enemyType.SetActive(true);
+        }
+
+    }
+    public void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            enemyType.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using AnimationManager;
 public class HealthBase : MonoBehaviour
 {
     public float startLife = 50f;
@@ -8,15 +10,33 @@ public class HealthBase : MonoBehaviour
     public float _currentLife;
     public Action<HealthBase> OnDamage;
     public Action<HealthBase> OnKill;
+    public Collider collider;
+    public FlashColor flashColor;
+    public bool lookAtPlayer = false;
+    public ParticleSystem particleSystem;
+    [Header(" Animation")]
+    [SerializeField] public AnimationBase animationBase;
+    public float startAnimationDuration = .2f;
+    public Ease startAnimationEase = Ease.OutBack;
+    public bool startwithBornAnimation = true;
+    private Player _player;
     private void Awake()
     {
+        if (particleSystem != null) particleSystem.transform.SetParent(null);
+
         Init();
     }
-
+    public void Start()
+    {
+        _player = GameObject.FindObjectOfType<Player>();
+    }
     protected virtual void Init()
     {
         ResetLife();
+        if (startwithBornAnimation)
+            BornAnimation();
     }
+
     public void ResetLife()
     {
        
@@ -29,15 +49,23 @@ public class HealthBase : MonoBehaviour
             Destroy(gameObject, 3f);
 
         OnKill?.Invoke(this);
-    }
+        if (collider != null) collider.enabled = false;
+        Invoke("PlayParticle", 3f);
+        PlayAnimationByTrigger(AnimationType.DEATH);
 
-   [NaughtyAttributes.Button]
+    }
+    public void PlayParticle()
+    {
+        if (particleSystem != null) particleSystem.Play();
+    }
+    [NaughtyAttributes.Button]
    public void Damage()
     {
         Damage(5);
     }
     public void Damage(float f)
     {
+        if (flashColor != null) flashColor.Flash();
 
         _currentLife = -f;
         if (_currentLife <= 0)
@@ -47,5 +75,32 @@ public class HealthBase : MonoBehaviour
         OnDamage?.Invoke(this);
 
     }
+    #region ANIMATION
+    private void BornAnimation()
+    {
+        transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+    }
 
+    public void PlayAnimationByTrigger(AnimationType animationType)
+    {
+        animationBase.PlayAnimationByTrigger(animationType);
+    }
+    #endregion
+    private void OnCollisionEnter(Collision collision)
+    {
+        Player p = collision.transform.GetComponent<Player>();
+        if (p != null)
+        {
+            p.Damage(1);
+        }
+
+    }
+
+    public virtual void Update()
+    {
+        if (lookAtPlayer)
+        {
+            transform.LookAt(_player.transform.position);
+        }
+    }
 }
