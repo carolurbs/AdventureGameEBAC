7cea8cb5003c970fe40073ecaf7eb5ed38c2aa3d
CarolUrbs <caroline.urbainski@edu.univali.br>
2023-02-02 08:40:18 -0300

    Itens pt2
    Itens se destroem e dropam moedas

--- Assets/Scripts/Itens/DestructableItem/DestrutableItemBase.cs
+++ Assets/Scripts/Itens/DestructableItem/DestrutableItemBase.cs
@@ -9,7 +9,9 @@ public class DestrutableItemBase : MonoBehaviour
     public float shakeDuration = .1f;
     public int shakeForce= 5;
     public List<FlashColor> flashColors;
-
+    public int dropItemAmount = 10;
+    public GameObject coinPrefab;
+    public Transform dropPosition;
     public void OnValidate()
     {
         if (healthBase ==null) healthBase =GetComponent<HealthBase>();
@@ -27,7 +29,14 @@ public class DestrutableItemBase : MonoBehaviour
     {
       gameObject.transform.DOShakeScale(shakeDuration, Vector3.up/2, shakeForce);
         flashColors.ForEach(i => i.Flash());
+        DropCoins();
 
     }
-
+    [NaughtyAttributes.Button]
+    private void  DropCoins()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = transform.position;
+        i.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From(); 
+    }
 }
