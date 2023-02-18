895d7b879902c5cc4012d069f79e3ddc791c1c2a
CarolUrbs <caroline.urbainski@edu.univali.br>
2023-02-17 21:16:57 -0300

    ui

--- Assets/Scripts/Sound/MuteSound.cs
+++ Assets/Scripts/Sound/MuteSound.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MuteSound : MonoBehaviour
 {
     public AudioSource musicPlayer;
     public List<AudioSource> sfx;
+    public List<Image> soundOn;
+    public List<Image> soundOff;
+    public List<Image> musicOn;
+    public List<Image> musicOff;
     public bool _isMusicMute;
     public bool _isSFXMute;
     public void Awake()
@@ -54,21 +58,27 @@ public class MuteSound : MonoBehaviour
     public void MusicMute()
     {
         musicPlayer.enabled = false;
+        musicOff.ForEach(i => i.enabled = false);
+        musicOn.ForEach(i => i.enabled = true);
     }
     public void MusicUnmute()
     {
         musicPlayer.enabled = true;
-
+        musicOff.ForEach(i => i.enabled = true);
+        musicOn.ForEach(i => i.enabled = false);
     }
     public void SFXMute()
     {
         sfx.ForEach(i => i.enabled = false);
+        soundOff.ForEach(i => i.enabled=false);
+        soundOn.ForEach(i => i.enabled = true);
 
     }
     public void SFXUnmute()
     {
         sfx.ForEach(i => i.enabled = true);
-
+        soundOff.ForEach(i => i.enabled = true);
+        soundOn.ForEach(i => i.enabled = false);
     }
 }
 
